b92f99a2488f4eac4dcd040ed1933f8854a14299
TJAlston <tjalston82@gmail.com>
2016-11-30 15:01:09 -0500

    about us sub links added

--- wp-content/themes/page.php
+++ wp-content/themes/page.php
@@ -7,6 +7,14 @@ if (have_posts()) :
 ?>
 
 <article class="post page">
+
+  <?php
+  $args = array(
+  'child_of' => get_top_ancestor_id(),
+  'title_li' => ''
+  /* gets rid of header PAGES*/
+  );?>
+  <?php wp_list_pages($args); ?>
   <h2> <?php the_title(); ?></h2>
   <?php the_content(); ?>
 </article>
